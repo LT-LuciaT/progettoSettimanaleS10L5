c82bf86fb4c93a4bf3971914bbdfdab414d3aa75
LT-LuciaT <lucia.trombin98@gmail.com>
2025-02-14 16:48:34 +0100

    6

--- src/components/Dettagli.jsx
+++ src/components/Dettagli.jsx
@@ -1,20 +1,28 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Card, Col } from "react-bootstrap";
+import "../App.css";
+import { Card, Container, Row } from "react-bootstrap";
 
 const Dettagli = () => {
   const { lat, lon } = useParams();
   const [forecastData, setForecastData] = useState(null);
+  const [MeteoGiornaliero, setMeteoGiornaliero] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchForecast = async () => {
+    const fetchData = async () => {
       try {
         const response = await fetch(
           `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=eee1f68d87654a0046257c0175f570f6`
         );
         const data = await response.json();
         setForecastData(data);
+        const meteoCorrente = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=eee1f68d87654a0046257c0175f570f6`
+        );
+        const meteoCorrenteData = await meteoCorrente.json();
+        setMeteoGiornaliero(meteoCorrenteData);
+
         setError(null);
       } catch (error) {
         console.error("Errore durante il recupero delle previsioni:", error);
@@ -22,14 +30,14 @@ const Dettagli = () => {
       }
     };
 
-    fetchForecast();
+    fetchData();
   }, [lat, lon]);
 
   if (error) {
     return <p>{error}</p>;
   }
 
-  if (!forecastData) {
+  if (!forecastData || !MeteoGiornaliero) {
     return <p>Caricamento...</p>;
   }
   const dailyForecasts = forecastData.list.filter((forecast) => {
@@ -37,36 +45,93 @@ const Dettagli = () => {
     return date.getHours() === 12;
   });
 
+  const sunsetTime = new Date(MeteoGiornaliero.sys.sunset * 1000).toLocaleTimeString();
+  const sunriseTime = new Date(MeteoGiornaliero.sys.sunrise * 1000).toLocaleTimeString();
+
   return (
-    <Col className="d-flex justify-content-center">
-      <Card className="mt-4">
+    <Container>
+      <Row>
         <Card.Body>
-          <Card.Title>Previsioni per i prossimi 5 giorni</Card.Title>
-          {dailyForecasts.map((forecast, index) => (
-            <div key={index}>
-              <Card.Text>
-                <strong>Data:</strong> {new Date(forecast.dt_txt).toLocaleDateString()}
-              </Card.Text>
-              <Card.Text>
-                <strong>Temperatura:</strong> {(forecast.main.temp - 273.15).toFixed(2)}°C
-              </Card.Text>
-              <Card.Text>
-                <strong>Condizioni:</strong> {forecast.weather[0].description}
-              </Card.Text>
-              <Card.Text>
-                <strong>Tempo:</strong>{" "}
-                <img
-                  src={`http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`}
-                  alt={forecast.weather[0].description}
-                  style={{ width: "30px", height: "auto" }}
-                />
-              </Card.Text>
-              <hr />
-            </div>
-          ))}
+          <Card.Title>Oggi</Card.Title>
+          <Card.Text>
+            <strong>Città:</strong> {MeteoGiornaliero.name}
+          </Card.Text>
+          <Card.Text>
+            <strong>Temperatura:</strong> {(MeteoGiornaliero.main.temp - 273.15).toFixed(2)}°C
+          </Card.Text>
+          <Card.Text>
+            <strong>Temperatura minima:</strong> {(MeteoGiornaliero.main.temp_min - 273.15).toFixed(2)}°C
+          </Card.Text>
+          <Card.Text>
+            <strong>Temperatura massima:</strong> {(MeteoGiornaliero.main.temp_max - 273.15).toFixed(2)}°C
+          </Card.Text>
+          <Card.Text>
+            <strong>Pressione:</strong> {MeteoGiornaliero.main.pressure} hPa
+          </Card.Text>
+          <Card.Text>
+            <strong>Umidità:</strong> {MeteoGiornaliero.main.humidity}%
+          </Card.Text>
+          <Card.Text>
+            <strong>Condizioni:</strong> {MeteoGiornaliero.weather[0].description}
+          </Card.Text>
+          <Card.Text>
+            <strong>Icona:</strong>{" "}
+            <img
+              src={`http://openweathermap.org/img/w/${MeteoGiornaliero.weather[0].icon}.png`}
+              alt={MeteoGiornaliero.weather[0].description}
+              style={{ width: "30px", height: "auto" }}
+            />
+          </Card.Text>
+          <Card.Text>
+            <strong>Alba:</strong> {sunriseTime}
+          </Card.Text>
+          <Card.Text>
+            <strong>Tramonto:</strong> {sunsetTime}
+          </Card.Text>
         </Card.Body>
-      </Card>
-    </Col>
+      </Row>
+
+      <h2>Previsioni per i prossimi 5 giorni</h2>
+      <Row>
+        {dailyForecasts.map((forecast, index) => (
+          <Container key={index} className="d-flex justify-content-center">
+            <Card className="flex-wrap">
+              <Card.Body>
+                <Card.Text>
+                  <strong>Data:</strong> {new Date(forecast.dt_txt).toLocaleDateString()}
+                </Card.Text>
+                <Card.Text>
+                  <strong>Temperatura:</strong> {(forecast.main.temp - 273.15).toFixed(2)}°C
+                </Card.Text>
+                <Card.Text>
+                  <strong>Temperatura minima:</strong> {(forecast.main.temp_min - 273.15).toFixed(2)}°C
+                </Card.Text>
+                <Card.Text>
+                  <strong>Temperatura massima:</strong> {(forecast.main.temp_max - 273.15).toFixed(2)}°C
+                </Card.Text>
+                <Card.Text>
+                  <strong>Pressione:</strong> {forecast.main.pressure} hPa
+                </Card.Text>
+                <Card.Text>
+                  <strong>Umidità:</strong> {forecast.main.humidity}%
+                </Card.Text>
+                <Card.Text>
+                  <strong>Condizioni:</strong> {forecast.weather[0].description}
+                </Card.Text>
+                <Card.Text>
+                  <strong>Icona:</strong>{" "}
+                  <img
+                    src={`http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`}
+                    alt={forecast.weather[0].description}
+                    style={{ width: "30px", height: "auto" }}
+                  />
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Container>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
